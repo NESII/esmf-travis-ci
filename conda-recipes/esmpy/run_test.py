7bc7de56f17fd5bdea156bf5e318b90fe3ad0bf3
bekozi <benkoziol@gmail.com>
2017-10-24 08:12:51 -0600

    Working ESMPy testing

--- conda-recipes/esmpy/run_test.py
+++ conda-recipes/esmpy/run_test.py
@@ -1,18 +1,13 @@
-import os
 import subprocess
 
-import ESMF
+# Serial tests.
+cmd1 = ['nosetests', '-vs', '-a', '!slow,!parallel,!data', 'ESMF']
 
-try:
-    import mpi4py
-    has_mpi4py = True
-except ImportError:
-    has_mpi4py = False
+# Parallel tests.
+cmd2 = ['mpirun', '-n', '4', 'nosetests', '-vs', '-a', '!slow,!serial,!data', 'ESMF']
 
-if ESMF.__version__.startswith('7') and os.uname()[0] != 'Darwin':
-    if has_mpi4py:
-        cmd = ['mpirun', '-np', '4']
-    else:
-        cmd = []
-    cmd.extend(['nosetests', '-vs', '-a', '!slow,!data,serial', 'ESMF'])
+# Slow tests.
+# cmd3 = ['nosetests', '-vs', '-a', 'slow,!data', 'ESMF']
+
+for cmd in [cmd1, cmd2]:
     subprocess.check_call(cmd)
