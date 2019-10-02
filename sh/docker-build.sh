a272ea44616a1722687fad009007d28921f3e00d
bekozi <benkoziol@gmail.com>
2019-10-02 11:48:06 -0600

    Minor changes to naming conventions

--- sh/docker-build.sh
+++ sh/docker-build.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+set -eO pipefail
 
-cd ..
-#docker build --pull --no-cache -t esmf .
-docker build -t esmf .
+cd ../docker
+
+cd esmf-ubuntu
+docker build -t bekozi/esmf-ubuntu .
+
+cd ../esmf-hdf5
+docker build -t bekozi/esmf-hdf5 .
+
+cd ../esmf-netcdf
+docker build -t bekozi/esmf-netcdf .
+
+docker push bekozi/esmf-netcdf
